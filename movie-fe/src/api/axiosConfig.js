d703257e6078d8010666816a7acf13dab084bfb3
Trung Nguyen <nguyencraft1314@gmail.com>
2024-05-01 13:47:44 +0700

    change be docker file and env

--- movie-fe/src/api/axiosConfig.js
+++ movie-fe/src/api/axiosConfig.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
-const baseURL = process.env.BASE_URL; // Default value if not set
+const baseURL = process.env.REACT_APP_BASE_URL; // Default value if not set
 
 export default axios.create({
     baseURL: baseURL,
 });
-
-console.log(baseURL)
-console.log(process.env.BASE_URL)
